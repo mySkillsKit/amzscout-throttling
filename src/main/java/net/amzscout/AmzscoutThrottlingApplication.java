09d76b29aa3d36f70356a06481b6e84c2d38496a
Anatolii Vasilev <avas2012@gmail.com>
2023-06-22 10:52:13 +0300

    first commit

---
+++ src/main/java/net/amzscout/AmzscoutThrottlingApplication.java
@@ -0,0 +1,13 @@
+package net.amzscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmzscoutThrottlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmzscoutThrottlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/amzscout/AmzscoutThrottlingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.amzscout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmzscoutThrottlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
